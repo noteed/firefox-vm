97008fc63df75f3bfd00d5ea2813f56a3cabece6
Vo Minh Thu <thu@hypered.io>
2024-04-05 15:47:23 +0200

    Use openbox, and a 2560x1440 resolution.
    We increase the resolution of the QEMU window.
    
    Without a window manager, Firefox doesn't seem to know it can fill more
    room on the screen, and some menus behave weirdly.

--- machine/configuration.nix
+++ machine/configuration.nix
@@ -1,24 +1,53 @@
 { config, pkgs, ... }:
 
+
+let
+  autostart = ''
+    #!${pkgs.bash}/bin/bash
+    # End all lines with '&' to not halt startup script execution
+
+    firefox --kiosk https://refli.be/ &
+  '';
+
+in
 {
-  # Enable X11 without a display manager, running Firefox directly
+  # Overlay to set custom autostart script for openbox
+  # From the answer at
+  # https://discourse.nixos.org/t/how-to-configure-nixos-for-kiosk-or-fullscreen-applications/21855/2
+  nixpkgs.overlays = with pkgs; [
+    (_self: super: {
+      openbox = super.openbox.overrideAttrs (_oldAttrs: rec {
+        postFixup = ''
+          ln -sf /etc/openbox/autostart $out/etc/xdg/openbox/autostart
+        '';
+      });
+    })
+  ];
+
+  # By defining the script source outside of the overlay, we don't have to
+  # rebuild the package every time we change the startup script.
+  environment.etc."openbox/autostart".source = pkgs.writeScript "autostart" autostart;
+
+  # Enable X11 without a display manager
   services.xserver = {
     enable = true;
-    displayManager.defaultSession = "custom";
-    displayManager.session = [
-      {
-        manage = "desktop";
-        name = "custom";
-        start = ''
-          exec firefox --kiosk https://refli.be
-        '';
-      }
-    ];
+    windowManager.openbox.enable = true;
+    displayManager.lightdm.enable = true;
+    displayManager.defaultSession = "none+openbox";
     displayManager.autoLogin = {
       enable = true;
       user = "user";
     };
+
+    layout = "us";
+    libinput.enable = true;
+    videoDrivers = [ "qxl" ]; # Otherwise we can't use the 2560x1440.
   };
+
+  systemd.services."display-manager".after = [
+    "network-online.target"
+    "systemd-resolved.service"
+  ];
 
   # Set up user environment
   users.users.user = {
@@ -32,4 +61,15 @@
 
   # Allow user to run system commands without a password
   # security.sudo.wheelNeedsPassword = false;
+
+  # Specify how to run QEMU
+  virtualisation.resolution = {
+    x = 2560;
+    y = 1440;
+  };
+  virtualisation.qemu.options = [
+    "-vga virtio"
+    "-display gtk"
+    "-device VGA,vgamem_mb=64"
+  ];
 }
