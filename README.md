674b938e06987a5c46f7ee8a34a2477ac1af8d0e
Vo Minh Thu <thu@hypered.io>
2024-11-30 15:59:14 +0100

    Update the VM definition.
    The displayed web page, is exactly the same as what is displayed on my
    Framework laptop.

--- README.md
+++ README.md
@@ -8,3 +8,39 @@ $ scripts/runvm.sh
 
 Use Ctrl-Alt-F to exit the fullscreen mode, and use Ctrl-Alt-Q to quit QEMU
 (this doesn't work while in fullscreen).
+
+# Notes
+
+The above `runvm.sh` script is already configured to do the "right things" but
+here is how I achieved it manually first.
+
+In the VM, to use 2880x1920:
+
+```
+$ gtf 2880 1920 60
+$ xrandr --newmode "2880x1920_60.00" 473.06 2880 3104 3424 3969 1920 1921 1924 1987 -HSync +Vsync
+$ xrandr --addmode Virtual-2 "2880x1920_60.00"
+$ xrandr --output Virtual-2 --mode "2880x1920_60.00"
+```
+
+The corresponding NixOS bits are
+
+```
+  services.xserver = {
+    monitorSection = ''
+      Modeline "2880x1920_60.00" 473.06 2880 3104 3424 3968 1920 1921 1924 1987 -HSync +Vsync
+      Option "PreferredMode" "2880x1920_60.00"
+    '';
+  };
+```
+
+In Firefox `about:config`: `layout.css.devPixelsPerPx` is set to 0.9. Then
+restart Firefox.
+
+The default font size is set to 14.
+
+In the console, `window.devicePixelRatio` equals 2.4. (When
+`layout.css.devPixelsPerPx` equals -1, it equals 2.60869).
+
+This means that on a fullscreen 2880x1920 window, Firefox reports a viewport of
+1200x800.
--- machine/configuration.nix
+++ machine/configuration.nix
@@ -4,9 +4,17 @@
 let
   autostart = ''
     #!${pkgs.bash}/bin/bash
-    # End all lines with '&' to not halt startup script execution
 
-    firefox --kiosk https://refli.be/ &
+    firefox -no-remote -CreateProfile "vm /home/user/.mozilla/firefox/vm"
+    cat <<EOF > "/home/user/.mozilla/firefox/vm/user.js"
+    user_pref("font.size.variable.x-western", 14);
+    user_pref("layout.css.devPixelsPerPx", "0.9");
+    EOF
+    firefox \
+      --kiosk \
+      --profile /home/user/.mozilla/firefox/vm/ \
+      --remote-debugging-port 9222 \
+      https://refli.be/ &
   '';
 
 in
@@ -24,9 +32,16 @@ in
     })
   ];
 
+  networking.firewall.allowedTCPPorts = [ 9222 ];
+
   # By defining the script source outside of the overlay, we don't have to
   # rebuild the package every time we change the startup script.
   environment.etc."openbox/autostart".source = pkgs.writeScript "autostart" autostart;
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.font = "ter-132n";
+  console.keyMap = "us";
+  console.packages = with pkgs; [ terminus_font ];
 
   # Enable X11 without a display manager
   services.xserver = {
@@ -42,8 +57,35 @@ in
     layout = "us";
     libinput.enable = true;
     videoDrivers = [ "qxl" ]; # Otherwise we can't use the 2560x1440.
-    dpi = 210;
+    dpi = 256;
+
+    monitorSection = ''
+      Modeline "2880x1920_60.00" 473.06 2880 3104 3424 3968 1920 1921 1924 1987 -HSync +Vsync
+      Option "PreferredMode" "2880x1920_60.00"
+    '';
   };
+
+  # TODO The two Xcursor lines don't seem to work, but maybe I need to restart the session.
+  # TODO Same for this line:
+  # Which is called fonts.optimizeForVeryHighDPI is later version of NixOS.
+  services.xserver.displayManager.sessionCommands = ''
+    xrdb "${pkgs.writeText "xrdb.conf" ''
+      XTerm*faceName:             xft:DejaVu Sans Mono for Powerline:size=8
+      XTerm*utf8:                 2
+
+      Xft.dpi: 256
+      Xft.autohint: 0
+      Xft.lcdfilter: lcddefault
+      Xft.hintstyle: hintfull
+      Xft.hinting: 1
+      Xft.antialias: 1
+      Xft.rgba: rgb
+
+      Xcursor.theme: Vanilla-DMZ
+      ! Apparently size only works if a theme exists?
+      Xcursor.size: 48
+    ''}"
+  '';
 
   systemd.services."display-manager".after = [
     "network-online.target"
@@ -65,13 +107,16 @@ in
 
   # Specify how to run QEMU
   virtualisation.resolution = {
-    x = 2560;
-    y = 1440;
+    x = 2880;
+    y = 1920;
   };
   virtualisation.qemu.options = [
     "-vga virtio"
     "-display gtk"
     "-device VGA,vgamem_mb=64"
     "-full-screen" # Use Ctrl-Alt-F to exit fullscreen.
+  ];
+  virtualisation.forwardPorts = [
+    { from = "host"; host.port = 9222; guest.port = 9222; }
   ];
 }
